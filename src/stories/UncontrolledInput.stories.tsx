49dcb0581301939291f69315361c0561a1f04812
Zubescu <asa82@inbox.ru>
2020-10-07 17:30:41 +0300

    controlled input

--- src/stories/UncontrolledInput.stories.tsx
+++ src/stories/UncontrolledInput.stories.tsx
@@ -28,4 +28,36 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     return <> <input ref={inputRef}/> <button onClick={save}>Save</button> - actual value: {value}</>
 }
 
-export const ControlledInput = () => <input value={'trololo'}/>
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+
+    return <input type='checkbox' checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('2')
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <select value={parentValue} onChange={onChange}>
+        <option value="1">none</option>
+        <option value="2">Moscow</option>
+        <option value="3">Odessa</option>
+    </select>
+}
